ef6f98cb997edfee1eb03415937f5b1324fef864
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-29 00:31:42 +0100

    saler ecommerce website

--- app/components/products/ReturnsandDelivery.tsx
+++ app/components/products/ReturnsandDelivery.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { TbTruckDelivery } from "react-icons/tb";
-import { GiReturnArrow } from "react-icons/gi";
+import { RiRefund2Fill } from "react-icons/ri";
+import { RiShieldStarFill } from "react-icons/ri";
 import { Horizontal } from "@/app/product/[productid]/ProductDetails";
 
 const ReturnsandDelivery = () => {
   return (
-    <div className="p-3 bg-[#FFFFFF] rounded-md flex flex-col">
+    <div className="p-3 bg-[#FFFFFF] rounded-md flex flex-col h-fit">
       <p>Returns and Delivery</p>
       <Horizontal />
       <div className="flex flex-col gap-1">
@@ -24,6 +25,26 @@ const ReturnsandDelivery = () => {
         </p>
         <p className=" text-[10px] max-w-[200px]">
           Note: Subject to availability in your location
+        </p>
+      </div>
+      <Horizontal />
+      <div className="flex flex-col gap-1">
+        <p className="flex items-center gap-1">
+          <RiRefund2Fill className="text-[#F89F24]" /> Return Policy
+        </p>
+        <p className="font-semibold text-base">7 Day Return Guarantee</p>
+        <p className=" text-[10px] max-w-[200px]">
+          For more information on the return shipping options, go to Konga
+          Return Policy
+        </p>
+      </div>
+      <Horizontal />
+      <div className="flex flex-col gap-1">
+        <p className="flex items-center gap-1">
+          <RiShieldStarFill className="text-[#F89F24]" /> Warranty
+        </p>
+        <p className=" text-[10px] max-w-[200px]">
+          Warranty information unavailable for this item.
         </p>
       </div>
     </div>
