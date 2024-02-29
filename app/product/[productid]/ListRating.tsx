288567b74f8c816aad18b350348da163eb82c659
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-29 09:51:06 +0100

    saler ecommerce website

--- app/product/[productid]/ListRating.tsx
+++ app/product/[productid]/ListRating.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
+import { Rating } from "@mui/material";
+import moment from "moment";
 import React from "react";
+import { Horizontal } from "./ProductDetails";
 
 interface ListRatingProps {
   product: any;
@@ -13,8 +16,24 @@ const ListRating: React.FC<ListRatingProps> = ({ product }) => {
       <Heading title="Product Reviews" />
       <div className="text-sm mt-2">
         {product.reviews &&
-          product.reviews.map(() => {
-            return;
+          product.reviews.map((review: any) => {
+            return (
+              <div key={review.id} className="max-w-300px">
+                <div className="flex gap-2 items-center">
+                  <div>Avatar</div>
+                  <div className=" font-semibold ">{review?.user.name}</div>
+                  <div className=" font-light">
+                    {moment(review.createdDate).fromNow()}
+                  </div>
+                </div>
+
+                <div className="mt-2 ">
+                  <Rating value={review.rating} readOnly />
+                  <div className="ml-2">{review.comment}</div>
+                  <Horizontal />
+                </div>
+              </div>
+            );
           })}
       </div>
     </div>
