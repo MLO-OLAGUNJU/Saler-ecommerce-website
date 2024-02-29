9c5cffad823e28b9b28c183c40166a5b30e1482a
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-29 15:58:54 +0100

    I built an ecommerce website called Saler

--- app/hook/useCart.tsx
+++ app/hook/useCart.tsx
@@ -1,7 +1,10 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useCallback, useContext, useState } from "react";
+import { CartProductType } from "../product/[productid]/ProductDetails";
 
 type CartContextType = {
   cartTotalQty: number;
+  cartProducts: CartProductType[] | null;
+  handleAddProductToCart: (product: CartProductType) => void;
 };
 
 interface Props {
@@ -12,9 +15,26 @@ export const CartContext = createContext<CartContextType | null>(null);
 
 export const CartContextProvider = (props: Props) => {
   const [cartTotalQty, setCartTotalQty] = useState(0);
+  const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(
+    null
+  );
 
+  const handleAddProductToCart = useCallback((product: CartProductType) => {
+    setCartProducts((prev) => {
+      let updatedCart;
+
+      if (prev) {
+        updatedCart = [...prev, product];
+      } else {
+        updatedCart = [product];
+      }
+      return updatedCart;
+    });
+  }, []);
   const value = {
     cartTotalQty,
+    cartProducts,
+    handleAddProductToCart,
   };
 
   return <CartContext.Provider value={value} {...props} />;
