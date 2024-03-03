1341a14c9fa6ce3c1e6f5470242bf361bb5ba730
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-03 19:56:49 +0100

    saler ecommerce website

---
+++ app/checkout/CheckoutClient.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useCart } from "../hook/useCart";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+const CheckoutClient = () => {
+  const { cartProducts, paymentIntent, handleSetPaymentIntent } = useCart();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [clientSecret, setClientSecret] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (cartProducts) {
+      setIsLoading(true);
+      setError(false);
+
+      fetch("api/create-payment-intent", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          items: cartProducts,
+          payment_intent_id: paymentIntent,
+        }),
+      })
+        .then((res) => {
+          setIsLoading(false);
+          if (res.status === 401) {
+            return router.push("/login");
+          }
+
+          return res.json();
+        })
+        .then((data) => {
+          setClientSecret(data.paymentIntent.client_secret);
+          handleSetPaymentIntent(data.paymentIntent.id);
+        })
+        .catch((error) => {
+          setError(true);
+          console.log("Error:", error);
+          toast.error("Something went wrong");
+        });
+    }
+  }, [cartProducts, paymentIntent]);
+
+  return <div></div>;
+};
+
+export default CheckoutClient;
