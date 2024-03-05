be6917b40891c9c81da4147ba91bfc620c94cbe7
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-05 18:22:17 +0100

    saler ecommerce website

--- app/checkout/CheckOutForm.tsx
+++ app/checkout/CheckOutForm.tsx
@@ -1,5 +1,8 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { useCart } from "../hook/useCart";
+import { useElements, useStripe } from "@stripe/react-stripe-js";
+import formatPrice from "@/utils/formatPrice";
 
 interface CheckOutFormProps {
   clientSecret: string;
@@ -9,6 +12,12 @@ const CheckOutForm: React.FC<CheckOutFormProps> = ({
   clientSecret,
   handlesetPaymentSuccess,
 }) => {
+  const { cartTotalAmount, handleClearCart, handleSetPaymentIntent } =
+    useCart();
+  const stripe = useStripe();
+  const elements = useElements();
+  const [loading, setLoading] = useState(false);
+  const formattedPrice = formatPrice(cartTotalAmount);
   return (
     <>
       <div>CheckOutForm</div>
