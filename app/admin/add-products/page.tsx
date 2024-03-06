c4592d54a81030b6dbefaa674f9fbb0a7bf0585a
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 00:12:28 +0100

    saler ecommerce website

--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import AddProductForm from "./AddProductForm";
+import Container from "@/app/components/Container";
 
 const AddProducts = () => {
-  return <div>AddProducts</div>;
+  return (
+    <div className="pt-8 text-[#0F1111] select-none ">
+      <Container>
+        <AddProductForm />
+      </Container>
+    </div>
+  );
 };
 
 export default AddProducts;
