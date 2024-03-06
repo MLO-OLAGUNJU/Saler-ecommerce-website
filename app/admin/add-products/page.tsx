9e0ff940467fd3a184236b8b6f736b56d99dd8a4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 00:19:32 +0100

    saler ecommerce website

--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import AddProductForm from "./AddProductForm";
 import Container from "@/app/components/Container";
+import FormWrap from "@/app/components/FormWrap";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
 
-const AddProducts = () => {
+const AddProducts = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops! Access Denied" />;
+  }
+
   return (
     <div className="pt-8 text-[#0F1111] select-none ">
       <Container>
-        <AddProductForm />
+        <FormWrap>
+          <AddProductForm />
+        </FormWrap>
       </Container>
     </div>
   );
---
+++ app/components/NullData.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface NullDataProps {
+  title: string;
+}
+const NullData: React.FC<NullDataProps> = ({ title }) => {
+  return (
+    <div className="w-full h-[50vh] items-center justify-center text-xl md:text-2xl">
+      <p className="font-medium">{title}</p>
+    </div>
+  );
+};
+
+export default NullData;
