3d46996ac533375408bbce7fbe6ad9f7f2d69aca
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 00:02:52 +0100

    saler ecommerce website

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,9 +1,12 @@
 import prisma from "@/libs/prismadb";
 import { NextResponse } from "next/server";
 import { getCurrentUser } from "@/actions/getCurrentUser";
+import Admin from "@/app/admin/page";
 
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
+  if (!currentUser || currentUser.role !== "ADMIN") {
+  }
   const body = await request.json();
   const { name, email, password } = body;
   const user = await prisma.user.create({
