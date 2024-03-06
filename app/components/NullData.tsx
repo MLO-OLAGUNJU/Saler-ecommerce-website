70295a7fca636770aa6d881f4f45259a7277f436
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 00:37:29 +0100

    saler ecommerce website

--- app/components/NullData.tsx
+++ app/components/NullData.tsx
@@ -5,7 +5,7 @@ interface NullDataProps {
 }
 const NullData: React.FC<NullDataProps> = ({ title }) => {
   return (
-    <div className="w-full h-[50vh] items-center justify-center text-xl md:text-2xl">
+    <div className="w-full h-[50vh] flex items-center justify-center text-xl md:text-2xl">
       <p className="font-medium">{title}</p>
     </div>
   );
