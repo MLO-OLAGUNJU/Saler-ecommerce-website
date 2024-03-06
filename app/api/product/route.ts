fbe787c017e0d51ccf5a24c666a8fdc470cfc5a0
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-06 23:59:18 +0100

    saler ecommerce website

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -3,14 +3,13 @@ import { NextResponse } from "next/server";
 import { getCurrentUser } from "@/actions/getCurrentUser";
 
 export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
   const body = await request.json();
   const { name, email, password } = body;
-  const hashedPassword = await bcrypt.hash(password, 10);
   const user = await prisma.user.create({
     data: {
       name,
       email,
-      hashedPassword,
     },
   });
   return NextResponse.json(user);
