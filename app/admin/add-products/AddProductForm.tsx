cca5964e5f2bb79699f5f772f536e19ae1f041fb
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 01:47:26 +0100

    saler ecommerce website

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,9 +1,65 @@
 "use client";
 
-import React from "react";
+import Heading from "@/app/components/Heading";
+import Input from "@/app/components/inputs/Input";
+import TextArea from "@/app/components/inputs/TextArea";
+import { error } from "console";
+import React, { useState } from "react";
+import { FieldValue, FieldValues, useForm } from "react-hook-form";
 
 const AddProductForm = () => {
-  return <div>AddProductForm</div>;
+  const [loading, setLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      description: "",
+      price: "",
+      brand: "",
+      category: "",
+      inStock: false,
+      images: [],
+      reviews: "",
+    },
+  });
+  return (
+    <>
+      <Heading title="Add a Product" />
+      <Input
+        id="name"
+        label="Name"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        required
+        autoFocus={true}
+      />
+      <Input
+        id="price"
+        label="Price"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        type="number"
+        required
+        autoFocus={false}
+      />
+      <TextArea
+        id="description"
+        label="Description"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </>
+  );
 };
 
 export default AddProductForm;
