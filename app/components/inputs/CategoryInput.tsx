b4baa04a12355d616a2a67b2adb2400f085672d6
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 02:32:53 +0100

    saler ecommerce website

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -1,7 +1,32 @@
+"use client";
+import { ClassNames } from "@emotion/react";
 import React from "react";
+import { IconType } from "react-icons";
 
-const CategoryInput = () => {
-  return <div>CategoryInput</div>;
+interface CategoryInpuProps {
+  selected?: boolean;
+  label: string;
+  icon: IconType;
+  onClick: (value: string) => void;
+}
+
+const CategoryInput: React.FC<CategoryInpuProps> = ({
+  selected,
+  label,
+  icon: Icon,
+  onClick,
+}) => {
+  return (
+    <div
+      onClick={() => onClick(label)}
+      className={` rounded-xl border-2 p-4 flex items-center gap-2 flex-col hover:border-slate-500 transition cursor-pointer ${
+        selected ? "border-slate-500" : "border-slate-200"
+      }`}
+    >
+      <Icon size={30} />
+      <div className=" font-medium">{label}</div>
+    </div>
+  );
 };
 
 export default CategoryInput;
