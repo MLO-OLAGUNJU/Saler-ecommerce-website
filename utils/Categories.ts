c2d29173c8c7fcac490b93a6686510e0579e0e58
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 01:55:43 +0100

    saler ecommerce website

---
+++ utils/Categories.ts
@@ -0,0 +1,33 @@
+import { MdOutlineKeyboard, MdStorefront, MdTv, MdWatch } from "react-icons/md";
+import { AiFillPhone, AiOutlineDesktop, AiOutlineLaptop } from "react-icons/ai";
+
+export const Categories = [
+  {
+    label: "All",
+    icon: MdStorefront,
+  },
+  {
+    label: "Phone",
+    icon: AiFillPhone,
+  },
+  {
+    label: "Laptop",
+    icon: AiOutlineLaptop,
+  },
+  {
+    label: "Desktop",
+    icon: AiOutlineDesktop,
+  },
+  {
+    label: "Watch",
+    icon: MdWatch,
+  },
+  {
+    label: "TV",
+    icon: MdTv,
+  },
+  {
+    label: "Accesories",
+    icon: MdOutlineKeyboard,
+  },
+];
