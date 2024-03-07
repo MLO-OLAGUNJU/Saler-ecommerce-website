bf5f5a4432e7b7af75b967ff140280b94d0db4cc
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 14:40:18 +0100

    saler ecommerce website

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -12,8 +12,14 @@ import { Categories } from "@/utils/Categories";
 import { colors } from "@/utils/Colors";
 import React, { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { getStorage } from "firebase/storage";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import toast from "react-hot-toast";
+import { FaSketch } from "react-icons/fa";
 
 export type ImageType = {
   color: string;
@@ -90,11 +96,67 @@ const AddProductForm = () => {
           if (item.images) {
             const fileName = new Date().getTime() + "-" + item.image.name;
             const storage = getStorage(firebaseApp);
+            const storageRef = ref(storage, `products/${fileName}`);
+            const uploadTask = uploadBytesResumable(storageRef, item.image);
+
+            await new Promise<void>((resolve, reject) => {
+              uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                  const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+                  console.log("Upload is " + progress + "% done");
+
+                  switch (snapshot.state) {
+                    case "paused":
+                      console.log("Upload is paused");
+                      break;
+                    case "running":
+                      console.log("Upload is running");
+                      break;
+                  }
+                },
+                (error) => {
+                  //Handle unsucessful uploads
+                  console.log("Upload error", error);
+                  reject(error);
+                },
+                () => {
+                  //Handle successful uploads on complete
+                  //For instance, get the download URL: https://firebasestorage.googleapis.com/...
+
+                  getDownloadURL(uploadTask.snapshot.ref)
+                    .then((downloadURL) => {
+                      uploadedImages.push({
+                        ...item,
+                        image: downloadURL,
+                      });
+                      console.log("File available at ", downloadURL);
+
+                      resolve();
+                    })
+                    .catch((error) => {
+                      console.log("Error getting the downloadable URL", error);
+                      reject(error);
+                    });
+                }
+              );
+            });
           }
         }
-      } catch (error) {}
+      } catch (error) {
+        setLoading(false);
+        console.log("Error handling image uploads", error);
+        return toast.error("Error handling image uploads");
+      }
     };
+
+    await handleImageUpload();
+    const productData = { ...data, images: uploadedImages };
+    console.log(productData);
   };
+
   const category = watch("category");
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value),
