dadaa680b1a30c3796cec62f5501e22670dce16a
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-08 00:02:49 +0100

    saler ecommerce website

--- app/admin/manage-orders/page.tsx
+++ app/admin/manage-orders/page.tsx
@@ -1,7 +1,25 @@
+import Container from "@/app/components/Container";
 import React from "react";
+import ManageOrdersClient from "./ManageOrdersClient";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+import getOrders from "@/actions/getOrders";
 
-const ManageOrders = () => {
-  return <div>ManageOrders</div>;
+const ManageOrders = async () => {
+  const orders = await getOrders();
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops! Access Denied" />;
+  }
+
+  return (
+    <div className="p-8 bg-white text-[#0F1111] select-none ">
+      <Container>
+        <ManageOrdersClient orders={orders} />
+      </Container>
+    </div>
+  );
 };
 
 export default ManageOrders;
