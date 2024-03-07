ae44e8c8bf3ed35f88431b617f41e2ca71a8617e
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 23:24:31 +0100

    saler ecommerce website

--- app/admin/manage-products/ManageProductsClient.tsx
+++ app/admin/manage-products/ManageProductsClient.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import { Product } from "@prisma/client";
-import React from "react";
+import React, { useCallback } from "react";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-import { product } from "@/utils/product";
 import formatPrice from "@/utils/formatPrice";
 import Heading from "@/app/components/Heading";
+import Status from "@/app/components/Status";
+import {
+  MdCached,
+  MdClose,
+  MdDelete,
+  MdDone,
+  MdRemoveRedEye,
+} from "react-icons/md";
+import ActionBtn from "@/app/components/ActionBtn";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { error } from "console";
+import { deleteObject, getStorage, ref } from "firebase/storage";
 
 interface ManageProductsClientProps {
   products: Product[];
@@ -14,6 +27,8 @@ interface ManageProductsClientProps {
 const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
   products,
 }) => {
+  const router = useRouter();
+  const storage = getStorage();
   let rows: any = [];
 
   if (products) {
@@ -51,7 +66,23 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
       width: 120,
       renderCell: (params) => {
         return (
-          <div>{params.row.inStock === true ? "in stock" : "out of stock"}</div>
+          <div>
+            {params.row.inStock === true ? (
+              <Status
+                text="in stock"
+                icon={MdDone}
+                bg="bg-teal-200"
+                color="text-teal-700"
+              />
+            ) : (
+              <Status
+                text="out of stock"
+                icon={MdClose}
+                bg="bg-rose-200"
+                color="text-rose-700"
+              />
+            )}
+          </div>
         );
       },
     },
@@ -60,10 +91,55 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
       headerName: "Actions",
       width: 240,
       renderCell: (params) => {
-        return <div>Action</div>;
+        return (
+          <div className="flex justify-between gap-4 w-full">
+            <ActionBtn
+              icon={MdCached}
+              onClick={() => {
+                handleToggleStock(params.row.id, params.row.inStock);
+              }}
+            />
+            <ActionBtn icon={MdDelete} onClick={() => {}} />
+            <ActionBtn icon={MdRemoveRedEye} onClick={() => {}} />
+          </div>
+        );
       },
     },
   ];
+
+  const handleToggleStock = useCallback((id: string, inStock: boolean) => {
+    axios
+      .put("/api/product", {
+        id,
+        inStock: !inStock,
+      })
+      .then((res) => {
+        toast.success("Product status changed");
+        router.refresh();
+      })
+      .catch((error) => {
+        toast.error("Oops! Something went wrong");
+        console.log(error);
+      });
+  }, []);
+
+  const handleDelete = useCallback((id: string, images: any[]) => {
+    toast("Deleting product, please wait.....");
+
+    const handleImageDelete = async () => {
+      try {
+        for (const item of images) {
+          if (item.image) {
+            const imageRef = ref(storage, item.image);
+            await deleteObject(imageRef);
+            console.log("Successfully deleted Image!", item.image);
+          }
+        }
+      } catch (error) {
+        return console.log("Deleting images error", error);
+      }
+    };
+  }, []);
   return (
     <div className=" max-w-[1150px] m-auto text-xl">
       <div className=" mb-4 mt-8">
@@ -81,6 +157,7 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
           }}
           pageSizeOptions={[9, 20]}
           checkboxSelection
+          disableRowSelectionOnClick
         />
       </div>
     </div>
