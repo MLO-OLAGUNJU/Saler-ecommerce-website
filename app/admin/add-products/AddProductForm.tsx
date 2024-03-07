48fb701fb2c8502f98cb8ba2e7230761fca3dca4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-07 02:41:03 +0100

    saler ecommerce website

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
+import CategoryInput from "@/app/components/inputs/CategoryInput";
 import CustomCheckbox from "@/app/components/inputs/CustomCheckbox";
 import Input from "@/app/components/inputs/Input";
 import TextArea from "@/app/components/inputs/TextArea";
 import { Categories } from "@/utils/Categories";
-import { error } from "console";
 import React, { useState } from "react";
-import { FieldValue, FieldValues, useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 
 const AddProductForm = () => {
   const [loading, setLoading] = useState(false);
@@ -30,6 +30,17 @@ const AddProductForm = () => {
       reviews: "",
     },
   });
+
+  const category = watch("category");
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value),
+      {
+        shouldValidate: true,
+        shouldDirty: true,
+        shouldTouch: true,
+      };
+  };
+
   return (
     <>
       <Heading title="Add a Product" />
@@ -69,11 +80,21 @@ const AddProductForm = () => {
 
       <div className=" w-full font-medium">
         <div className="mb-2 font-semibold">Select a Category</div>
-        <div className="grid grid-cols-2 md:grid-cols-3 max-h-[50vh] overflow-y-auto">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-3 max-h-[50vh] overflow-y-auto">
           {Categories.map((item) => {
             if (item.label === "All") {
               return null;
             }
+            return (
+              <div key={item.label} className="col-span">
+                <CategoryInput
+                  onClick={(category) => setCustomValue("category", category)}
+                  selected={category === item.label}
+                  label={item.label}
+                  icon={item.icon}
+                />
+              </div>
+            );
           })}
         </div>
       </div>
