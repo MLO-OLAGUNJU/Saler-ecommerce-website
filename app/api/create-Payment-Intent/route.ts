1146ebdb185ae3a3e75fe80ead1046e8ab4baed3
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-09 02:53:11 +0100

    saler ecommerce website

--- app/api/create-Payment-Intent/route.ts
+++ app/api/create-Payment-Intent/route.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import prisma from "@/libs/prismadb";
 import { NextResponse } from "next/server";
 import { getCurrentUser } from "@/actions/getCurrentUser";
-import { CartProductType } from "@/app/product/[productId]/ProductDetails";
+import type { CartProductType } from "@/app/product/[productId]/ProductDetails";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2023-10-16",
--- app/components/products/SetQauntity.tsx
+++ app/components/products/SetQauntity.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CartProductType } from "@/app/product/[productId]/ProductDetails";
+import type { CartProductType } from "@/app/product/[productId]/ProductDetails";
 import React from "react";
 
 interface setQtyProps {
