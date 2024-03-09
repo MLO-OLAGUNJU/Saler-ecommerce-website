abf07b06496c3d649ae31a3fdbd8816baaa4f9b3
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-09 22:34:25 +0100

    saler ecommerce website

--- utils/formatPrice.ts
+++
@@ -1,8 +0,0 @@
-export const formatPrice = (amount: number) => {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(amount);
-};
-
-export default formatPrice;
