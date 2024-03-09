968b78829020f78c18412adf3038445043989e74
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-09 23:45:19 +0100

    saler ecommerce website

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import Container from "../components/Container";
-import CartClient from "./CartClient";
-import { getCurrentUser } from "@/actions/getCurrentUser";
+// import React from "react";
+// import Container from "../components/Container";
+// import CartClient from "./CartClient";
+// import { getCurrentUser } from "@/actions/getCurrentUser";
 
-const Cart = async () => {
-  const currentUser = await getCurrentUser();
-  return (
-    <div className="pt-8 text-[#0F1111] select-none ">
-      <Container>
-        <CartClient currentUser={currentUser} />
-      </Container>
-    </div>
-  );
-};
+// const Cart = async () => {
+//   const currentUser = await getCurrentUser();
+//   return (
+//     <div className="pt-8 text-[#0F1111] select-none ">
+//       <Container>
+//         <CartClient currentUser={currentUser} />
+//       </Container>
+//     </div>
+//   );
+// };
 
-export default Cart;
+// export default Cart;
