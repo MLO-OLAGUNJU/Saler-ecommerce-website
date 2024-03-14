3ea6f8e0cc5d64799233d41ce3c05d100f7351c9
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-14 17:30:10 +0100

    saler ecommerce website

--- pages/api/stripe-webhooks.ts
+++ pages/api/stripe-webhooks.ts
@@ -1,6 +1,7 @@
 import { buffer } from "micro";
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
+import prisma from "@/libs/prismadb";
 
 export const config = {
   api: {
