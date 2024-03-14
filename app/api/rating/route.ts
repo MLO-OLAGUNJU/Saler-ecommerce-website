ec70d7cc41b128805af907a26ea020b1c66e4cbb
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-14 17:48:51 +0100

    saler ecommerce website

--- app/api/rating/route.ts
+++ app/api/rating/route.ts
@@ -1,6 +1,7 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import { Review } from "@prisma/client";
 import { NextResponse } from "next/server";
+import prisma from "@/libs/prismadb";
 
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
