c25b520672ad2583b0d93d6fa3f07ca4ea7dbc13
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-14 23:46:00 +0100

    saler ecommerce website

--- app/components/nav/SearchBar.tsx
+++ app/components/nav/SearchBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useRouter } from "next/navigation";
 import queryString from "query-string";
 import React from "react";
@@ -6,6 +7,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 const SearchBar = () => {
   const router = useRouter();
+
   const {
     register,
     handleSubmit,
@@ -20,7 +22,7 @@ const SearchBar = () => {
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     if (!data.searchTerm) return router.push("/");
 
-    const url = queryString.stringify(
+    const url = queryString.stringifyUrl(
       {
         url: "/",
         query: {
@@ -33,20 +35,22 @@ const SearchBar = () => {
     router.push(url);
     reset();
   };
+
   return (
     <div className="flex items-center">
       <input
         {...register("searchTerm")}
         autoComplete="off"
         type="text"
-        placeholder="Explore Saler.io"
-        className="p-2 border border-gray-300 rounded-l-md focus:outline-none focus:border-[0.5px] focus:border-slate-500 w-80 text-black"
+        placeholder="Find a product..."
+        className=" text-black p-2 border-gray-300 rounded-l-md focus:outline-none focus:border-[0.5px] focus:border-slate-500 w-80"
       />
+
       <button
         onClick={handleSubmit(onSubmit)}
-        className=" bg-slate-700 hover:opacity-80 text-white p-[9px] rounded-r-md"
+        className=" bg-slate-700 hover:opacity-80 text-white p-2 rounded-r-md"
       >
-        Search
+        Find
       </button>
     </div>
   );
